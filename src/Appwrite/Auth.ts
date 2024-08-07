646ef5ff4e09752ac01dc2bb636326cfcba84d77
Aman Kumar <amanksh05@gmail.com>
2024-08-07 17:10:13 +0530

    Appwrite logic moved to context

--- src/Appwrite/Auth.ts
+++ src/Appwrite/Auth.ts
@@ -6,42 +6,44 @@ const client = new Client()
     
     const account = new Account(client);
 
-export const loginGoogle = async (): Promise<void> => {
-    try {
-        await account.createOAuth2Session(
-            OAuthProvider.Google,
-            'http://localhost:3000/home',
-            'http://localhost:3000/fail',
-        );
-        console.log('Login success')
-    } catch (error) {
-        console.log("Login failed")
-    }
-}
+// export const loginGoogle = async (): Promise<void> => {
+//     try {
+//         await account.createOAuth2Session(
+//             OAuthProvider.Google,
+//             'http://localhost:3000/home',
+//             'http://localhost:3000/fail',
+//         );
+//         console.log('Login success')
+//     } catch (error) {
+//         console.log("Login failed")
+//     }
+// }
 
-export const profile = async (): Promise<void> => {
-    try {
-        const profile = await account.get()
-        console.log(profile);
-    } catch (error) {
-        console.log("Failed to fetch the data");
-    }
+// export const profile = async (): Promise<void> => {
+//     try {
+//         const profile = await account.get()
+//         console.log(profile);
+//     } catch (error) {
+//         console.log("Failed to fetch the data");
+//     }
 
 
-}
+// }
 
-export const logout = async():Promise<void> =>{
-    try {
-        await account.deleteSession('current');
-    } catch (error) {
+// export const logout = async():Promise<void> =>{
+//     try {
+//         await account.deleteSession('current');
+//     } catch (error) {
         
-    }
-}
+//     }
+// }
 
-export const token = async ():Promise<void> =>{
-    const session = await account.getSession('current');
-    console.log(session.provider);
-    console.log(session.providerUid);
-    console.log(session.providerAccessToken);
-    console.log(session.providerAccessTokenExpiry);
-}
+// export const token = async ():Promise<void> =>{
+//     const session = await account.getSession('current');
+//     console.log(session.provider);
+//     console.log(session.providerUid);
+//     console.log(session.providerAccessToken);
+//     console.log(session.providerAccessTokenExpiry);
+// }
+
+export {client, account};
